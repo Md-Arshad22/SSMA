6b5224b67a615eb772affc99e07fcb92f7563156
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-01-27 03:56:36 +0530

    Update fs.js

--- fs.js
+++ fs.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
-fs.writeFileSync('read.txt','welcome Arshad');
-
 // const fs = require('fs');
-// fs.readFile('./arshad.txt','utf-8',(error, data) => {
-//     if(error) {
-//         console.log(error);
-//     }
-//     else{
-//         console.log(data);
-//     }
-// });
+// fs.writeFileSync('read.txt','welcome Arshad');
+
+const fs = require('fs');
+fs.readFile('./arshad.txt','utf-8',(error, data) => {
+    if(error) {
+        console.log(error);
+    }
+    else{
+        console.log(data);
+    }
+});
